aa5d2807b90140b5f27e8ebbdab9520a0aeba5c5
willhughes11 <willhughes2110@gmail.com>
2020-09-13 17:36:53 -0400

    1st commit

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Jason Schatz on 11/2/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set colorView on launch
+        changeColorComponent()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent() {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
